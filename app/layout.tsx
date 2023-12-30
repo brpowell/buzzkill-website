37d2dc9f07e0818898e61310e5652701c846e042
Bryan Powell <10779873+brpowell@users.noreply.github.com>
2023-12-30 14:53:19 -0500

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Slabo_27px } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Disclaimer from "./components/disclaimer";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Disclaimer />
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
