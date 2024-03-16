a036510356e5b6e0ffc2af3f1138cc26ab66fcb5
Bryan Powell <10779873+brpowell@users.noreply.github.com>
2024-03-16 12:50:40 -0500

    ugh something got messed up

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       <div className="flex flex-col items-center pt-8 sm:p-18">
         <Image
           src={"/logo.webp"}
+          className="mb-10 px-10"
           width={450}
           height={337}
           alt="buzzkill.tips - just tell me"
